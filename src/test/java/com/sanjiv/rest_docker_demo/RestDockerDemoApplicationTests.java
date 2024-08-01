dda72fa026cfcf0645d57014392ee49786a939df
sanjiv <sanjivkumarpandit11101998@gmail.com>
2024-08-01 06:45:38 +0530

    first commit

---
+++ src/test/java/com/sanjiv/rest_docker_demo/RestDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjiv.rest_docker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
